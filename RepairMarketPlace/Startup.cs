1e54908c9f98af7e731ad2c77be0d9372ccbb6e1
Labocania <hugokl@outlook.com>
2021-11-02 20:23:51 -0300

    Removed SQL Server Nuget Package. Added initial reference for PostgreSQL.

--- RepairMarketPlace/Startup.cs
+++ RepairMarketPlace/Startup.cs
@@ -28,8 +28,7 @@ namespace RepairMarketPlace
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
