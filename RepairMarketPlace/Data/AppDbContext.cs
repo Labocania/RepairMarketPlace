90cf74cbd799da56550cf4e626eb25685b1aaec0
Labocania <hugokl@outlook.com>
2021-11-02 20:33:13 -0300

    Added extension method to configure PostgreSQL connection string. Renamed ApplicationDbContext to AppDbContext.

--- RepairMarketPlace/Data/AppDbContext.cs
+++ RepairMarketPlace/Data/AppDbContext.cs
@@ -6,9 +6,9 @@ using System.Text;
 
 namespace RepairMarketPlace.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class AppDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
         }
--- RepairMarketPlace/Startup.cs
+++ RepairMarketPlace/Startup.cs
@@ -17,21 +17,22 @@ namespace RepairMarketPlace
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Environment = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbProvider(Environment, Configuration);
+
             services.AddDatabaseDeveloperPageExceptionFilter();
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDbContext>();
             services.AddRazorPages();
         }
 
@@ -62,6 +63,45 @@ namespace RepairMarketPlace
             {
                 endpoints.MapRazorPages();
             });
+        }
+    }
+
+    public static class AddDbProviderExtensions
+    {
+        // Reference: https://github.com/jincod/dotnetcore-buildpack/issues/33#issuecomment-409935057
+        public static IServiceCollection AddDbProvider(this IServiceCollection services, IWebHostEnvironment env, IConfiguration config)
+        {
+            string connStr = "";
+            if (env.EnvironmentName == "Development")
+            {
+                connStr = config.GetConnectionString("DefaultConnection");
+            }
+
+            if (env.EnvironmentName == "Production")
+            {
+                string connUrl = config.GetSection("DATABASE_URL").Value;
+
+                // Parse connection URL to connection string for Npgsql
+                connUrl = connUrl.Replace("postgres://", string.Empty);
+
+                string pgUserPass = connUrl.Split("@")[0];
+                string pgHostPortDb = connUrl.Split("@")[1];
+                string pgHostPort = pgHostPortDb.Split("/")[0];
+
+                string pgDb = pgHostPortDb.Split("/")[1];
+                string pgUser = pgUserPass.Split(":")[0];
+                string pgPass = pgUserPass.Split(":")[1];
+                string pgHost = pgHostPort.Split(":")[0];
+                string pgPort = pgHostPort.Split(":")[1];
+
+                connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};sslmode=Prefer;Trust Server Certificate=true";
+            }
+
+            services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connStr));
+
+            //services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connStr,
+                //o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
+            return services;
         }
     }
 }
