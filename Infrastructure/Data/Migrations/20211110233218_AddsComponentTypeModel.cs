36cab74cf5452a26271ad2f12eef00acb7bfdb26
Labocania <hugokl@outlook.com>
2021-11-10 20:32:39 -0300

    Adds migration files.

---
+++ Infrastructure/Data/Migrations/20211110233218_AddsComponentTypeModel.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Web.Migrations
+{
+    public partial class AddsComponentTypeModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Components",
+                newName: "ComponentTypeId");
+
+            migrationBuilder.CreateTable(
+                name: "ComponentTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComponentTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Components_ComponentTypeId",
+                table: "Components",
+                column: "ComponentTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Components_ComponentTypes_ComponentTypeId",
+                table: "Components",
+                column: "ComponentTypeId",
+                principalTable: "ComponentTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Components_ComponentTypes_ComponentTypeId",
+                table: "Components");
+
+            migrationBuilder.DropTable(
+                name: "ComponentTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Components_ComponentTypeId",
+                table: "Components");
+
+            migrationBuilder.RenameColumn(
+                name: "ComponentTypeId",
+                table: "Components",
+                newName: "Type");
+        }
+    }
+}
